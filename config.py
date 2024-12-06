a0affca9e2129d6986f9cc01ad9709f312ddce2c
Pablo Lino <118943316+PabloLino@users.noreply.github.com>
2024-12-06 16:27:34 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,1 @@
-import pyodbc
 
-connection_string = (
-    "Driver={ODBC Driver 11 for SQL Server};"
-    "Server=*******;"  
-    "Database=DB_BackupOn_Manager;"  
-    "UID=****;"  
-    "PWD=E***;"  
-)
-
-try:
-    conn = pyodbc.connect(connection_string)
-    print("Conexão bem-sucedida!")
-    conn.close()
-except Exception as e:
-    print(f"Erro na conexão: {str(e)}")
